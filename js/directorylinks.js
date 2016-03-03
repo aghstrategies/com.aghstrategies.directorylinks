3b88faf7850e5f43ffc491edc03226fc06957710
Alice Frumin <alice@aghstrategies.com>
2016-03-03 12:32:36 -0500

    reformatted phones to use microformat

--- js/directorylinks.js
+++ js/directorylinks.js
@@ -6,14 +6,26 @@ CRM.$(function ($) {
     $(this).wrapInner("<a href='mailto:" + $(this).text() + "' target='_blank'></a>");
   });
 
+  $('tr').addClass('vcard');
   $('.crm-phone').each(function () {
-    $(this).wrapInner("<a href='tel:" + $(this).text() + "'></a>");
+    $(this).wrapInner("<span class='tel'></span>");
   });
 
-  // for page view
+  // for page view email
   var primaryEmail = $('#row-email_Primary').find('.content');
-  var primaryPhone = $('#row-phone_Primary_1').find('.content');
+  var emailLink = $('<a/>', {
+    href: 'mailto:' + $(primaryEmail).html(),
+    target: '_blank',
+    text: $(primaryEmail).html(),
+  });
+  $(primaryEmail).html(emailLink);
 
-  $(primaryEmail).wrapInner("<a href='mailto:" + $(primaryEmail).html() + "' target='_blank'></a>");
-  $(primaryPhone).wrapInner("<a href='tel:" + $(primaryPhone).html() + "'></a>");
+  // for page view phone
+  $('.crm-profile-view').addClass('vcard');
+  var primaryPhone = $('#row-phone_Primary_1').find('.content');
+  var phoneLink = $('<span/>', {
+    class: 'tel',
+    text: $(primaryPhone).html(),
+  });
+  $(primaryPhone).html(phoneLink);
 });
